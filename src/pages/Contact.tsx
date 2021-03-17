2874c93e791dfcb40f7015d0164559684719c7af
Ismael Muñoz <Prosilver05@gmail.com>
2021-03-17 01:44:26 -0600

    changed a lot of styling stuff, but most importantly, made it optimized por phone

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -64,13 +64,13 @@ export const Contact : React.FC = () => {
                     <p>Mensaje</p>
                   </div>
                   <Button
-                  size="lg"
-                  leftIcon={<PlaneIcon/>}
-                  isLoading={isButtonLoading}
-                  loadingText="Enviando"
-                  onClick={SendData}
-                  colorScheme="whatsapp"
-                  variant="outline"
+                    size="lg"
+                    leftIcon={<PlaneIcon />}
+                    isLoading={isButtonLoading}
+                    loadingText="Enviando"
+                    onClick={SendData}
+                    colorScheme="whatsapp"
+                    variant="outline"
                   >
                     Enviar
                   </Button>
@@ -78,9 +78,7 @@ export const Contact : React.FC = () => {
               </div>
             </div>
 
-            <div className="MapContainer">
-
-            </div>
+            <div className="MapContainer"></div>
           </form>
         </div>
 
@@ -90,12 +88,11 @@ export const Contact : React.FC = () => {
           </div>
 
           <div className="Icons">
-            <span>
+            <span className="Whats">
               <svg
                 onClick={() =>
                   window.open("https://wa.me/527714116235", "_newtab")
                 }
-                className="Whats"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
                 height="24"
@@ -104,12 +101,11 @@ export const Contact : React.FC = () => {
                 <path d="M12.031 6.172c-3.181 0-5.767 2.586-5.768 5.766-.001 1.298.38 2.27 1.019 3.287l-.582 2.128 2.182-.573c.978.58 1.911.928 3.145.929 3.178 0 5.767-2.587 5.768-5.766.001-3.187-2.575-5.77-5.764-5.771zm3.392 8.244c-.144.405-.837.774-1.17.824-.299.045-.677.063-1.092-.069-.252-.08-.575-.187-.988-.365-1.739-.751-2.874-2.502-2.961-2.617-.087-.116-.708-.94-.708-1.793s.448-1.273.607-1.446c.159-.173.346-.217.462-.217l.332.006c.106.005.249-.04.39.298.144.347.491 1.2.534 1.287.043.087.072.188.014.304-.058.116-.087.188-.173.289l-.26.304c-.087.086-.177.18-.076.354.101.174.449.741.964 1.201.662.591 1.221.774 1.394.86s.274.072.376-.043c.101-.116.433-.506.549-.68.116-.173.231-.145.39-.087s1.011.477 1.184.564.289.13.332.202c.045.072.045.419-.1.824zm-3.423-14.416c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm.029 18.88c-1.161 0-2.305-.292-3.318-.844l-3.677.964.984-3.595c-.607-1.052-.927-2.246-.926-3.468.001-3.825 3.113-6.937 6.937-6.937 1.856.001 3.598.723 4.907 2.034 1.31 1.311 2.031 3.054 2.03 4.908-.001 3.825-3.113 6.938-6.937 6.938z" />
               </svg>
             </span>
-            <span>
+            <span className="Twit">
               <svg
                 onClick={() =>
                   window.open("mailto:smortmc2@gmail.com", "_newtab")
                 }
-                className="Twit"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
                 height="24"
@@ -118,15 +114,14 @@ export const Contact : React.FC = () => {
                 <path d="M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.505l7 5.673 7-5.672v8.504z" />
               </svg>
             </span>
-            <span>
+            <span className="Face">
               <svg
                 onClick={() =>
                   window.open(
-                    "https://www.facebook.com/Smort-mc2-104848164862837/?view_public_for=104848164862837",
+                    "https://www.facebook.com/voadesmexico",
                     "_newtab"
                   )
                 }
-                className="Face"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
                 height="24"
@@ -135,12 +130,11 @@ export const Contact : React.FC = () => {
                 <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />
               </svg>
             </span>
-            <span>
+            <span className="Insta">
               <svg
                 onClick={() =>
                   window.open("https://www.instagram.com/smortmc2/", "_newtab")
                 }
-                className="Insta"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
                 height="24"
